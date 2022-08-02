22169933ccc00b8535a551659abb7996bf4ff1ec
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-08-02 22:42:35 +0300

    apibranch07

--- server.js
+++ server.js
@@ -1,45 +1,63 @@
-'use strict'
-const weatherData = require('./data/weather.json')
-
-const express = require('express');
-const cors = require('cors');
+'use strict';
+const express = require ('express');
+const cors = require ('cors');
 require('dotenv').config();
-const server = express();
+//---
+const weather =require('./data/weather.json');
+const PORT = process.env.PORT;
+const server = express ();
 server.use(cors());
 
-
-const PORT = process.env.PORT;
-
-server.get('/weather', (req, res) => {
-try{
-    let cityWeatherData = weatherData.find(city => city.city_name.toLowerCase() === req.query.searchQuery.toLowerCase())
-    let cityWeatherDataDays = cityWeatherData.data.map(day => {
-        let forecastDay = new Forecast(day, day.weather.description)
-        return forecastDay;
-    });
-    res.send(cityWeatherDataDays);
-
-}
-catch(error){
-    res.send(error)
-
-}
-
-
+server.get('/', (req,res)=>{
+    res.send('home route')
 })
 
 
-server.get('*',(req,res)=>{
-    res.status(500).send("!my port app working ..")
+
+server.get('/weather', handleWeather);
+
+
+function handleWeather(request, response){
+   
+        let x = request.query.x;
+        const result = weather.find((item) =>{
+        
+                if(item.city_name.toLowerCase()===x.toLowerCase()){
+                    console.log('hello from inside')
+                    
+                    return item
+
+                }
+            
+        })
+    try{   
+     const weatherArray = result.data.map(day => new Forecast(day))
+     response.status(200).send(weatherArray.description)
+    console.log(weatherArray)
+    }catch(error){
+        errorHandler(error, response)
+    }          
+
+        
+       
+ 
+    
+}
+
+ function Forecast(day){
+     this.date=day.datetime
+     this.description = day.weather.description 
+
+ }
+
+function errorHandler(error, response) {
+    response.status(500).send(`something went wrong ==> ${error}`);
+}
+
+server.get('*',(rquest,response) =>{
+    response.status(400).send('not found')
 })
 
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-
-
-
-class Forecast {
-    constructor(data, description) {
-        this.data = data;
-        this.description = description;
-    }
-}
+server.listen(PORT , ()=>{
+    console.log(`Listrning to PORT ${PORT}`)
+})
