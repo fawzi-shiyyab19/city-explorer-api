37d03b4912be970d84873027c610b470d927d916
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-08-06 23:13:23 +0300

    lab10

--- module/movie.js
+++ module/movie.js
@@ -1,15 +1,23 @@
 const axios =require('axios');
+const moviesCache = {};
 
 async function handleMovie (req,res)  {
+
     const searchQuery=req.query.searchQuery;
     const movieArr=await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`);
-    try{
-const movieData=movieArr.data.results.map(item => new Movie(item));
-res.status(200).send(movieData)
-    }catch(error){
-errorHandle(error,res)
-    }
-}
+    if (moviesCache[cityName] !== undefined) {
+                console.log('comes from Our Server');
+                response.status(200).send(moviesCache[cityName]);
+            } else {
+                    const moviesData = await axios.get(url);
+                    const arrayOfMoviesData = moviesData.data.results.map(item => {
+                        return new MovieApi(item);
+                    });
+                    console.log('comes from Outer API');
+                    moviesCache[cityName] = arrayOfMoviesData;
+                    response.status(200).send(arrayOfMoviesData);
+            };
+        }
 
 class Movie{
     constructor(movie){
@@ -23,3 +31,6 @@ class Movie{
     }
 }
 module.exports={handleMovie};
+
+
+
