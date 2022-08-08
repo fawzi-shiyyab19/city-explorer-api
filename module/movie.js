05657e01f95cc09de299a39df464c6fb2837af66
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-08-08 13:50:53 +0300

    lab9fixed

--- module/movie.js
+++ module/movie.js
@@ -1,36 +1,28 @@
-const axios =require('axios');
-const moviesCache = {};
+const axios = require('axios');
 
-async function handleMovie (req,res)  {
+async function handleMovie(req, res) {
 
-    const searchQuery=req.query.searchQuery;
-    const movieArr=await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`);
-    if (moviesCache[cityName] !== undefined) {
-                console.log('comes from Our Server');
-                response.status(200).send(moviesCache[cityName]);
-            } else {
-                    const moviesData = await axios.get(url);
-                    const arrayOfMoviesData = moviesData.data.results.map(item => {
-                        return new MovieApi(item);
-                    });
-                    console.log('comes from Outer API');
-                    moviesCache[cityName] = arrayOfMoviesData;
-                    response.status(200).send(arrayOfMoviesData);
-            };
-        }
-
-class Movie{
-    constructor(movie){
-        this.title= movie.title;
-    this.overview= movie.overview;
-    this.average_votes= movie.vote_average;
-    this.total_votes= movie.vote_count;
-    this.image_url= movie.poster_path;
-    this.popularity= movie.popularity;
-    this.released_on= movie.released_on;
+    try {
+        const searchQuery = req.query.searchQuery;
+        const movieArr = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`);
+        const movieData = movieArr.data.results.map(item => new Movie(item));
+        res.status(200).send(movieData)
+    } catch (error) {
+        errorHandle(error, res)
     }
 }
-module.exports={handleMovie};
-
-
-
+function errorHandle(error, res) {
+    res.status(500).send({ error: 'something error' })
+}
+class Movie {
+    constructor(movie) {
+        this.title = movie.title;
+        this.overview = movie.overview;
+        this.average_votes = movie.vote_average;
+        this.total_votes = movie.vote_count;
+        this.image_url = movie.poster_path;
+        this.popularity = movie.popularity;
+        this.released_on = movie.released_on;
+    }
+}
+module.exports = handleMovie;
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const cors=require('cors');
 const data=express();
 data.use(cors());
-const {handleWeather}=require('./module/weather');
-const {handleMovie}=require('./module/movie')
+const handleWeather=require('./module/weather');
+const handleMovie=require('./module/movie')
 const weatherData =require('./data/weather.json');
 const { application, response } = require('express');
 
@@ -13,9 +13,7 @@ data.get('/weather',handleWeather);
 data.get('/movie',handleMovie);
 
 data.get('*',(req,res) => {res.status(404).send('page not find')})
-function errorHandle(error,res){
-res.status(500).send({error: 'something error'})
-}
+
 
 
 data.listen(process.env.PORT, () => {
